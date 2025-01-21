7416663b97d020a39676b96bf419d93b79332c83
Hallvard Hetlelid <epichallvard@hotmail.com>
2025-01-21 22:12:49 +0100

    Added a different profile image

--- app/root.tsx
+++ app/root.tsx
@@ -30,6 +30,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="google-site-verification" content="CE56k68i2r7ztXchm1TSwy696SYwjDRPfc8IiexwYg4" />
         <Meta />
         <Links />
       </head>
